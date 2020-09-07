6850a2cdd7e9d1e52dc3d922d535f85dae003955
AshwiniJagdale <jagdaleashwini18@gmail.com>
2020-09-07 16:01:43 +0530

    initial commit

---
+++ typescript.js
@@ -0,0 +1,8 @@
+var a = 10;
+console.log(a);
+var c = 10;
+console.log(c);
+var b = 20;
+console.log(b);
+var d = "Ashwini";
+console.log(d);
---
+++ typescript.ts
@@ -0,0 +1,11 @@
+var a : number = 10;
+console.log(a);
+
+var c : Number = 10;
+console.log(c);
+
+var b : Number = 20;
+console.log(b);
+
+var d : string ="Ashwini";
+console.log(d)
